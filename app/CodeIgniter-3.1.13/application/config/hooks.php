1c693da80d6609ad2e2f42cbf9f35d9ebfc44376
Gate-Yossi <124479603+Gate-Yossi@users.noreply.github.com>
2023-04-09 16:25:36 +0900

    note 2023.04.06

--- app/CodeIgniter-3.1.13/application/config/hooks.php
+++ app/CodeIgniter-3.1.13/application/config/hooks.php
@@ -11,3 +11,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/userguide3/general/hooks.html
 |
 */
+
+$hook['pre_controller'] = array(
+    'class'    => 'MyTidewaysXhprof',
+    'function' => 'enable',
+    'filename' => 'tideways_xhprof.php',
+    'filepath' => 'hooks',
+    'params'   => array()
+);
+
+$hook['post_controller'] = array(
+    'class'    => 'MyTidewaysXhprof',
+    'function' => 'disable',
+    'filename' => 'tideways_xhprof.php',
+    'filepath' => 'hooks',
+    'params'   => array()
+);
