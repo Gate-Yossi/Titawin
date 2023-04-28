420cd7f8801e7bd8306b69c3454db3cf811c59e1
Gate-Yossi <124479603+Gate-Yossi@users.noreply.github.com>
2023-04-28 21:22:49 +0900

    add indexWithCache function

--- app/web/CodeIgniter-3.1.13/application/controllers/Sample.php
+++ app/web/CodeIgniter-3.1.13/application/controllers/Sample.php
@@ -13,4 +13,26 @@ class Sample extends CI_Controller {
 		}
 		echo 'Total Results: ' . $query->num_rows();
 	}
+
+	public function indexWithCache()
+	{
+		$this->load->driver('cache');
+
+		$cacheKey  = 'cache_key';
+		$cacheTTL = 3600;
+
+		$cacheData = $this->cache->memcached->get($cacheKey);
+		if ($cacheData === FALSE) {
+			$this->load->database();
+			$query = $this->db->query('SELECT name FROM sample');
+			$cacheData = $query->result();
+			$this->cache->memcached->save($cacheKey, $cacheData, $cacheTTL);
+		}
+
+		foreach ($cacheData as $row)
+		{
+			echo $row->name . '<br />';
+		}
+		echo 'Total Results: ' . count($cacheData);
+	}
 }
