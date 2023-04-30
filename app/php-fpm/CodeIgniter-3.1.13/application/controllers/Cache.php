0573034975c10e2f0c2c0df76b2db3aaaaba86b7
Gate-Yossi <124479603+Gate-Yossi@users.noreply.github.com>
2023-04-30 14:25:53 +0900

    add redis library to php

--- app/php-fpm/CodeIgniter-3.1.13/application/controllers/Cache.php
+++ app/php-fpm/CodeIgniter-3.1.13/application/controllers/Cache.php
@@ -3,6 +3,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cache extends CI_Controller {
 
+	public function redis()
+	{
+		$this->load->driver('cache');
+
+		$cacheKey  = 'cache_key';
+		$cacheData = 'data_to_be_cached';
+		$cacheTTL = 3600;
+
+		$ret = $this->cache->redis->save($cacheKey, $cacheData, $cacheTTL);
+		echo sprintf('save is %s <br />', $ret ? 'success' : 'fail');
+
+		$data = $this->cache->redis->get($cacheKey);
+		echo sprintf('data is [%s] <br />', $data);
+
+		$ret = $this->cache->redis->delete($cacheKey);
+		echo sprintf('delete is %s <br />', $ret ? 'success' : 'fail');
+
+		$data = $this->cache->redis->get($cacheKey);
+		echo sprintf('data is [%s] <br />', $data);
+
+	}
+
 	public function memcached()
 	{
 		$this->load->driver('cache');
